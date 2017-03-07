21b4c5e3866a2e719f5539972e2e57d3b6bc7511
Tyler Coles <shidobu@gmail.com>
2017-03-07 12:29:13 -0500

    updating project

--- config.js
+++ config.js
@@ -1,10 +1,4 @@
 System.config({
-  //use typescript for compilation
-  transpiler: 'typescript',
-  //typescript compiler options
-  typescriptOptions: {
-    emitDecoratorMetadata: true
-  },
   //map tells the System loader where to look for things
   map: {
     app: "./src"
@@ -12,8 +6,8 @@ System.config({
   //packages defines our app package
   packages: {
     app: {
-      main: './main.ts',
-      defaultExtension: 'ts'
+      main: './main.js',
+      defaultExtension: 'js'
     }
   }
 });
